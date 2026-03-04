6d8b7fe40029662c961096e45a6aeb629b9bd89c
neilroman <neilromanmartinez@gmail.com>
2026-03-04 05:33:25 +0100

    Create main Flutter app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SonicStreamyApp());
+  runApp(MyApp());
 }
 
-class SonicStreamyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sonic Streamy',
+      title: 'SonicStreamy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MainTabView(),
-    );
-  }
-}
-
-class MainTabView extends StatefulWidget {
-  @override
-  _MainTabViewState createState() => _MainTabViewState();
-}
-
-class _MainTabViewState extends State<MainTabView> {
-  int _currentIndex = 0;
-
-  final List<Widget> _children = [
-    RadioTab(),
-    YouTubeTab(),
-  ];
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Sonic Streamy')), 
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.radio),
-            label: 'Radio',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.video_library),
-            label: 'YouTube',
-          ),
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to SonicStreamy!'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
       ),
     );
-  }
-}
-
-class RadioTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Radio Tab')); // Replace with your Radio UI
-  }
-}
-
-class YouTubeTab extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('YouTube Tab')); // Replace with your YouTube UI
   }
 }
